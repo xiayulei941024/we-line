830be155a07bc6e96e85618bdc9cea18ee7dfb98
xiayulei <xiayulei@indusforce.com>
2021-11-30 14:51:26 +0800

    进度条

--- we-line-admin/src/router/index.ts
+++ we-line-admin/src/router/index.ts
@@ -1,22 +1,46 @@
-import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
+import {
+  createRouter,
+  createWebHashHistory,
+  RouteRecordRaw,
+  RouteLocationNormalized,
+  NavigationGuardNext
+} from "vue-router";
+//进度条
+import NProgress from "nprogress";
+import "nprogress/nprogress.css";
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: "/login",
     name: "Login",
-    component: () =>
-      import("../views/login/Login.vue")
+    component: () => import("../views/login/Login.vue")
   },
   {
     path: "/",
-    component: () =>
-      import("../layout/Layout.vue")
+    component: () => import("../layout/Layout.vue")
   }
 ];
 
 const router = createRouter({
   history: createWebHashHistory(),
   routes
+});
+
+// 前置守卫
+router.beforeEach(
+  (
+    to: RouteLocationNormalized,
+    from: RouteLocationNormalized,
+    next: NavigationGuardNext
+  ) => {
+    NProgress.start();
+    next();
+  }
+);
+// 后置守卫
+router.afterEach((to: any, from: any, next: any) => {
+  NProgress.done();
+  next;
 });
 
 export default router;
